9fc0987a708d4584deebb8c945e6831d7b6ba74b
thomascsd <thomascsd808@gmail.com>
2020-01-04 20:14:04 +0800

    hash password

--- src/server/services/memberService.ts
+++ src/server/services/memberService.ts
@@ -1,13 +1,17 @@
+import { genSalt, hash } from 'bcrypt';
 import { RestDbService } from './restDbService';
 import { Member } from '../../shared/models/member';
 import { classToPlain } from 'class-transformer';
 
 export class MemberService {
   dbService: RestDbService = new RestDbService();
-  saveMember(member: Member) {
+  async saveMember(member: Member) {
     const data = classToPlain(member);
+    const salt = await genSalt();
+    const bPwd = await hash(data['password'], salt);
 
     data['birthday'] = `${member.birthdayYear}-${member.birthdayMonth}-${member.birthdayDay}`;
+    data['password'] = bPwd;
 
     return this.dbService.saveData('member', data);
   }
