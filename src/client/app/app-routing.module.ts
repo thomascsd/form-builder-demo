fe99bde0820c088eddecd347d493f5d25bffea7a
thomascsd <thomascsd808@gmail.com>
2020-01-29 16:32:52 +0800

    client: update order component

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { MemberComponent } from './member/member.component';
 import { LayoutComponent } from './layout/layout.component';
 import { ListComponent } from './list/list.component';
+import { OrderComponent } from './order/order.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'member', pathMatch: 'full' },
@@ -11,7 +12,8 @@ const routes: Routes = [
     component: LayoutComponent,
     children: [
       { path: 'member', component: MemberComponent },
-      { path: 'list', component: ListComponent }
+      { path: 'list', component: ListComponent },
+      { path: 'order', component: OrderComponent }
     ]
   }
 ];
--- src/client/app/order/order.component.ts
+++ src/client/app/order/order.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { DynamicFormBuilder, DynamicFormGroup } from 'ngx-dynamic-form-builder';
 import { County } from '../../../shared/models/county';
 import { Distinct } from '../../../shared/models/distinct';
 import { CountyService } from './county.service';
+import { Order } from '../../../shared/models/order';
 
 @Component({
   selector: 'app-order',
@@ -12,10 +14,17 @@ import { CountyService } from './county.service';
 export class OrderComponent implements OnInit {
   county$: Observable<County[]>;
   distinct$: Observable<Distinct[]>;
+  group: DynamicFormGroup<Order>;
 
-  constructor(private countyServie: CountyService) {}
+  constructor(private countyServie: CountyService, private fb: DynamicFormBuilder) {
+    this.group = this.fb.group(Order);
+  }
 
   ngOnInit() {
     this.county$ = this.countyServie.getCounties();
+  }
+
+  onCountyChange(county: County) {
+    this.distinct$ = this.countyServie.getDistincts(county.countyCode);
   }
 }
