557c4ec1738393e2b88b305cec241cc65dbbe074
thomascsd <thomascsd808@gmail.com>
2024-06-16 10:46:47 +0800

    list改寫

--- src/client/app/list/list.component.ts
+++ src/client/app/list/list.component.ts
@@ -1,27 +1,22 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MemberQuery } from '../core/state/member.query';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatSort } from '@angular/material/sort';
 import { MemberService } from '../core/state/member.service';
 import { Member } from '../../../shared/models/member';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
   displayedColumns: string[] = ['name', 'email', 'mobile', 'birthday'];
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
-  dataSource: MatTableDataSource<Member>;
+  members: Member[] = [];
 
   constructor(private memberQuery: MemberQuery, private memberService: MemberService) {}
 
   ngOnInit() {
     this.memberService.get().subscribe((members: Member[]) => {
-      this.dataSource = new MatTableDataSource<Member>(members);
-      this.dataSource.sort = this.sort;
+      this.members = members;
     });
-    //    const members = this.memberQuery.getAll();
   }
 }
